2fcc2e19adb4bc247250f533dea6c41f19d2daf1
oktaytu1 <oktayt6@gmail.com>
2023-03-14 17:57:39 +0300

    Web-Hastane2

---
+++ hastane/anaSayfa.php
@@ -0,0 +1,285 @@
+<!doctype html>
+<html lang="tr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hastane Uygulaması</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
+
+
+
+
+    <header>
+        <div class="px-3 py-2 text-bg-dark">
+          <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+              <a href="" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-hospital" viewBox="0 0 16 16">
+                    <path d="M8.5 5.034v1.1l.953-.55.5.867L9 7l.953.55-.5.866-.953-.55v1.1h-1v-1.1l-.953.55-.5-.866L7 7l-.953-.55.5-.866.953.55v-1.1h1ZM13.25 9a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5ZM13 11.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5Zm.25 1.75a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5Zm-11-4a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 3 9.75v-.5A.25.25 0 0 0 2.75 9h-.5Zm0 2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5ZM2 13.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5Z"/>
+                    <path d="M5 1a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1a1 1 0 0 1 1 1v4h3a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h3V3a1 1 0 0 1 1-1V1Zm2 14h2v-3H7v3Zm3 0h1V3H5v12h1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3Zm0-14H6v1h4V1Zm2 7v7h3V8h-3Zm-8 7V8H1v7h3Z"/>
+                  </svg>
+                  <h5>Hastane</h5>
+              </a>
+              
+              <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+                
+                <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                      </svg>
+    
+
+
+            <!--   Hangi giriş yapıldıysa onun adı-->  
+            <?php 
+            
+            include("kaynak/baglanti.php");
+            session_start();
+
+            if ($_SESSION["unvan"]=="doktor") {
+              $bul="select ilac from doktor";
+              $kayit=$baglanti->query($bul);
+            
+            }
+
+            else if ($_SESSION["unvan"]=="eczaci") {
+              $bul="select ilac from eczaci";
+              $bulUst="select ilac from doktor";
+
+              $kayit=$baglanti->query($bul);
+              $kayitUst=$baglanti->query($bulUst);
+              
+            }
+            
+            else if ($_SESSION["unvan"]=="hemsire") {
+              $bul="select ilac from hemsire";
+              $bulUst="select ilac from eczaci";
+
+              $kayit=$baglanti->query($bul);
+              $kayitUst=$baglanti->query($bulUst);
+            
+            }
+
+
+
+
+            echo ''.$_SESSION["unvan"].'';
+            ?>
+
+            <!--     -->
+
+            
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="px-3 py-2 border-bottom mb-3">
+          <div class="container d-flex flex-wrap justify-content-end">
+    
+            <div class="text-end">
+
+            <!--  giris.html sayfasına yönlendirecek   -->
+            <form>
+              <button type="button" class="btn btn-primary">Çıkış Yap</button>
+            </form>
+            <!--     -->
+
+            </div>
+          </div>
+        </div>
+      </header>
+
+
+
+
+  
+        <div class="container marketing mt-5 ">
+
+        <?php 
+        //doktor girişi yapıldığında sayfa içeriği
+          if($_SESSION["unvan"]=="doktor")
+          {
+            echo 'doktor
+          <div class="row">
+            <div class="col-lg-6 bg-light">
+              
+        <form>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">İlacı İste</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="İlaç Adı Girin">
+
+            <button class="w-50 py-2 mb-2 btn btn-outline-dark rounded-1" type="submit">İlacı İste</button>
+
+          </div>
+            </div>
+            <div class="col-lg-6">
+              
+          <div class="form-group">
+
+            <label for="exampleFormControlSelect1">İstenmiş İlaçlar</label>
+
+
+
+            <!--  veri tabanındaki ilaçları tabloda gösteriyor  -->
+            <select class="form-control" id="exampleFormControlSelect1">
+            ';
+            if ($kayit->num_rows>0) {
+              while ($satir=$kayit->fetch_assoc()) {
+                echo '<option>'. $satir["ilac"]   .'</option>';
+              }
+            }
+            echo '
+            </select>
+
+
+
+
+
+          </div>
+        </form>
+            </div>
+  
+        </div>
+
+        ';
+      }
+      
+        //eczaci girişi yapıldığında sayfa içeriği
+        if($_SESSION["unvan"]=="eczaci")
+        {
+          echo 'eczaci
+        <div class="row">
+          <div class="col-lg-4 bg-light">
+            
+      <form>
+        <div class="form-group">
+          <label for="exampleFormControlInput1">İlacı Stokta Göster</label>
+          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="İlaç Adı Girin">
+
+          <button class="w-50 py-2 mb-2 btn btn-outline-dark rounded-1" type="submit">İlacı Stokta Göster</button>
+
+        </div>
+          </div>
+          <div class="col-lg-4 ">
+            
+        <div class="form-group">
+
+          <label for="exampleFormControlSelect1">Doktorun İstediği İlaçlar</label>
+
+          <select class="form-control" id="exampleFormControlSelect1">
+          ';
+          if ($kayitUst->num_rows>0) {
+            while ($satir=$kayitUst->fetch_assoc()) {
+              echo '<option>'. $satir["ilac"]   .'</option>';
+            }
+          }
+          echo '
+
+          </select>
+        </div>
+        
+          </div>
+          
+          <div class="col-lg-4 bg-light">
+            
+        <div class="form-group">
+
+          <label for="exampleFormControlSelect1">Stoktaki İlaçlar</label>
+
+          <select class="form-control" id="exampleFormControlSelect1">
+          ';
+          if ($kayit->num_rows>0) {
+            while ($satir=$kayit->fetch_assoc()) {
+              echo '<option>'. $satir["ilac"]   .'</option>';
+            }
+          }
+          echo '
+          </select>
+        </div>
+        
+          </div>
+          
+      </form>
+      </div>
+      ';
+    }
+
+    
+        //hemsire girişi yapıldığında sayfa içeriği
+        if($_SESSION["unvan"]=="hemsire")
+        {
+          echo 'hemsire
+        
+          <div class="row ">
+          <div class="col-lg-4 bg-light">
+            
+      <form>
+        <div class="form-group">
+          <label for="exampleFormControlInput1">İlacı Al</label>
+          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="İlaç Adı Girin">
+
+          <button class="w-50 py-2 mb-2 btn btn-outline-dark rounded-1" type="submit">İlacı Al</button>
+
+        </div>
+          </div>
+          <div class="col-lg-4">
+            
+        <div class="form-group">
+
+          <label for="exampleFormControlSelect1">Stoktaki İlaçlar</label>
+
+          <select class="form-control" id="exampleFormControlSelect1">
+
+          ';
+          if ($kayitUst->num_rows>0) {
+            while ($satir=$kayitUst->fetch_assoc()) {
+              echo '<option>'. $satir["ilac"]   .'</option>';
+            }
+          }
+          echo '
+
+          </select>
+        </div>
+        
+          </div>
+          
+          <div class="col-lg-4 bg-light">
+            
+        <div class="form-group">
+
+          <label for="exampleFormControlSelect1">Alınmış İlaçlar</label>
+
+          <select class="form-control" id="exampleFormControlSelect1">
+
+          ';
+          if ($kayit->num_rows>0) {
+            while ($satir=$kayit->fetch_assoc()) {
+              echo '<option>'. $satir["ilac"]   .'</option>';
+            }
+          }
+          echo '
+
+          </select>
+        </div>
+        
+          </div>
+          
+      </form>
+      </div>
+      ';
+    }
+?>
+      </div>
+
+
+
+
+
+
+
+  </body>
+</html>
---
+++ hastane/giris.php
@@ -0,0 +1,142 @@
+<?php
+include("kaynak/baglanti.php");
+
+if (isset($_POST["submit"]) ) 
+{ 
+	$unvan=$_POST["unvan"];
+
+	if ($unvan=="") {
+		echo '<div >Gerekli Alanları Boş Bırakmayınız!</div>';
+	}
+	else{
+	$secim="SELECT * FROM kullanici WHERE unvan='$unvan'";
+	$calistir=mysqli_query($baglanti,$secim);
+	$kayitSayisi=mysqli_num_rows($calistir);
+
+
+	if ($kayitSayisi>0) {
+		session_start();
+		$ilgiliKayit=mysqli_fetch_assoc($calistir);
+		$_SESSION["unvan"]=$ilgiliKayit["unvan"];
+		header("location:anaSayfa.php");
+	}
+	else{
+		echo '<div>Böyle Bir Kullanıcı bulunamadı!</div>';
+	}
+
+
+	mysqli_close($baglanti);
+	}
+}
+
+
+
+
+
+
+else if(isset($_POST["submit1"]))
+{
+  session_start();
+  $_SESSION["unvan"]="doktor";
+  header("location:anaSayfa.php");
+}
+
+else if(isset($_POST["submit2"]))
+{
+  session_start();
+  $_SESSION["unvan"]="eczaci";
+  header("location:anaSayfa.php");
+}
+else if(isset($_POST["submit3"]))
+{
+  session_start();
+  $_SESSION["unvan"]="hemsire";
+  header("location:anaSayfa.php");
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!doctype html>
+<html lang="tr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hastane Uygulaması</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
+    <header>
+        <div class="modal modal-signin position-static d-block bg-secondary py-5" tabindex="-1" role="dialog" id="modalSignin">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content rounded-4 shadow">
+                <div class="modal-header p-5 pb-4 border-bottom-0">
+                  
+                  <h1 class="fw-bold mb-0 fs-2">Giriş Yap</h1>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-hospital" viewBox="0 0 16 16">
+                    <path d="M8.5 5.034v1.1l.953-.55.5.867L9 7l.953.55-.5.866-.953-.55v1.1h-1v-1.1l-.953.55-.5-.866L7 7l-.953-.55.5-.866.953.55v-1.1h1ZM13.25 9a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5ZM13 11.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5Zm.25 1.75a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5Zm-11-4a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 3 9.75v-.5A.25.25 0 0 0 2.75 9h-.5Zm0 2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5ZM2 13.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25v-.5Z"/>
+                    <path d="M5 1a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1a1 1 0 0 1 1 1v4h3a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h3V3a1 1 0 0 1 1-1V1Zm2 14h2v-3H7v3Zm3 0h1V3H5v12h1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3Zm0-14H6v1h4V1Zm2 7v7h3V8h-3Zm-8 7V8H1v7h3Z"/>
+                  </svg>
+                </div>
+          
+                <div class="modal-body p-5 pt-0">
+                  <form class="" action="giris.php" method="POST">
+                    <div class="form-floating mb-3">
+                      <input name="unvan" type="text" class="form-control rounded-3" id="floatingInput" placeholder="Kullanıcı Adı">
+                      <label for="floatingInput">Kullanıcı Adı</label>
+                    </div>
+                    <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit" name="submit">Giriş Yap</button>
+                    
+                    <hr class="my-4">
+                    <h2 class="fs-5 fw-bold mb-3">Hızlı Giriş</h2>
+
+                    <!--  buradan aşağısı: kullanıcı adı girmek yerine direkt giriş için   -->
+
+                    <button class="w-100 py-2 mb-2 btn btn-outline-dark rounded-3" type="submit" name="submit1">
+                      Doktor
+                    </button>
+                    <button class="w-100 py-2 mb-2 btn btn-outline-dark rounded-3" type="submit" name="submit2">
+                      Eczacı
+                    </button>
+                    <button class="w-100 py-2 mb-2 btn btn-outline-dark rounded-3" type="submit" name="submit3">
+                      Hemşire
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+      </header>
+
+
+
+
+
+
+
+
+
+
+
+  </body>
+</html>
